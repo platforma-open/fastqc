73ef0d1f2e72cefb601b1684aac4931346c4a192
julenmendieta <jmendieta@milaboratories.com>
2025-02-11 11:19:46 +0100

    Fixed new block naming issues

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,4 +1,4 @@
-import { model } from '@platforma-open/milaboratories.FastQC.model';
+import { model } from '@platforma-open/milaboratories.fastqc.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
 
